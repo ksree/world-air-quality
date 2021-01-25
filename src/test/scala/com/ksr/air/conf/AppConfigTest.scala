e77982a21af9f9e73baa9a91c296db398e8a2627
Kapil <kapilsreed@gmail.com>
2021-01-25 22:06:54 +0100

    UPdate config

--- src/test/scala/com/ksr/air/conf/AppConfigTest.scala
+++ src/test/scala/com/ksr/air/conf/AppConfigTest.scala
@@ -8,6 +8,6 @@ class AppConfigTest extends AnyFlatSpec {
   val appConfig: AppConfig = AppConfig(Array.empty[String])
 
   "config" should "have the properties" in {
-    assert(appConfig.awsBucket == "s3a://noaa-ghcn-pds/csv/")
+    assert(appConfig.awsBucketName == "openaq-fetches")
   }
 }
